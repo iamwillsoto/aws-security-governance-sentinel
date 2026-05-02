3426d4df09c5919976885d8a6534ca16b54e461c
Will Soto <wsoto12@wgu.edu>
2026-05-02 14:41:39 -0400

    Encrypt SQS DLQs and refine Trivy policy exceptions

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,6 +1,7 @@
 resource "aws_sqs_queue" "lambda_dlq" {
   name                      = "${var.project_name}-lambda-dlq"
   message_retention_seconds = 1209600
+  sqs_managed_sse_enabled   = true
 
   tags = {
     Project = var.project_name
@@ -11,9 +12,29 @@ resource "aws_sqs_queue" "lambda_dlq" {
 resource "aws_sqs_queue" "eventbridge_dlq" {
   name                      = "${var.project_name}-eventbridge-dlq"
   message_retention_seconds = 1209600
+  sqs_managed_sse_enabled   = true
 
   tags = {
     Project = var.project_name
     Purpose = "eventbridge-target-failure-dlq"
   }
+}
+
+resource "aws_sqs_queue_policy" "eventbridge_dlq_policy" {
+  queue_url = aws_sqs_queue.eventbridge_dlq.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEventBridgeToSendMessages"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.eventbridge_dlq.arn
+      }
+    ]
+  })
 }
